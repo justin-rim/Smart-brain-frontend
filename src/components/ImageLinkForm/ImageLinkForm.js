6967e4882d676fbe878a41bbaac305d13ff25318
Justin <jeuirim@gmail.com>
2021-04-25 12:44:35 -0700

    added instruction on page

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -6,7 +6,10 @@ const ImageLinkForm = ({ onInputChange, onPictureSubmit }) => {
    return (
       <div>
          <p className = 'f3'>
-            {'This Magic Brain will detect faces in your pictures'}
+            {'This Magic Brain will detect faces in your pictures.'}
+         </p>
+         <p className = 'f3'>
+            {'Paste an image URL into the box below!'}
          </p>
          <div className = 'center'>
             <div className= 'form center pa4 br3 shadow-5'>
